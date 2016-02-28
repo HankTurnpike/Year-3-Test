a70e06e11b25e70107452fc45850d668c1ea7c76
PatMulvan <monkeyislandfanatic@gmail.com>
2016-02-28 14:25:31 +0000

    In progress super early alpha

--- app/src/main/java/com/example/patrick/myapplication/DisplayMessageActivity.java
+++ app/src/main/java/com/example/patrick/myapplication/DisplayMessageActivity.java
@@ -7,18 +7,27 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.DayViewFacade;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.DayViewDecorator;
+import com.prolificinteractive.materialcalendarview.spans.DotSpan;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
 
 public class DisplayMessageActivity extends AppCompatActivity {
 
@@ -35,6 +44,16 @@ public class DisplayMessageActivity extends AppCompatActivity {
         Intent intent = getIntent();
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
         layout=(RelativeLayout)findViewById(R.id.second_row);
+        final MaterialCalendarView calendarView = (MaterialCalendarView) layout.findViewById(R.id.calendarView);
+        Log.d("test","tesstset");
+        List<CalendarDay> days = calendarView.getSelectedDates();
+        for(CalendarDay day : days) {
+            Log.d("test", day.toString());
+        }
+
+        calendarView.addDecorators(new EventDecorator(15269888, days));
+
+
         int prevTextViewId =0;
         for(int i = 0; i < 3; i++)
         {
@@ -74,5 +93,26 @@ public class DisplayMessageActivity extends AppCompatActivity {
 
         }
         catch (IOException e){}*/
+    }
+    public class EventDecorator implements DayViewDecorator {
+
+        private final int color;
+        private final HashSet<CalendarDay> dates;
+
+
+        public EventDecorator(int color, Collection<CalendarDay> dates) {
+            this.color = color;
+            this.dates = new HashSet<>(dates);
+        }
+
+        @Override
+        public boolean shouldDecorate(CalendarDay day) {
+            return dates.contains(day);
+        }
+
+        @Override
+        public void decorate(DayViewFacade view) {
+            view.addSpan(new DotSpan(5, color));
+        }
     }
 }
