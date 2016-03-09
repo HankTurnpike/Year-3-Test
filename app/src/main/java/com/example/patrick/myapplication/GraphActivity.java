1180ba764defa9bd3f3baccf71c823772d812b56
ofriele <eoin.ofriel2@mail.dcu.ie>
2016-03-09 16:08:09 +0000

    Graph

--- app/src/main/java/com/example/patrick/myapplication/GraphActivity.java
+++ app/src/main/java/com/example/patrick/myapplication/GraphActivity.java
@@ -1,9 +1,18 @@
 package com.example.patrick.myapplication;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Paint;
+import android.graphics.Shader;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
@@ -12,11 +21,70 @@ import java.util.ArrayList;
 import java.util.Calendar;
 
 public class GraphActivity extends AppCompatActivity {
+    LineChart lineChart;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.graph_activity);
-        LineChart lineChart = (LineChart) findViewById(R.id.chart);
+        //========================Draw graph=====================================
+        drawLineGraph();
+    }
+
+    private void drawLineGraph() {
+        setupLineChart();
+
+        // Get the paint renderer to create the line shading.
+        Paint paint = lineChart.getRenderer().getPaintRender();
+        int [] colours = {Color.rgb(20, 74, 129), //Blue
+                Color.rgb(0, 72, 255),
+                Color.rgb(0, 144, 255),
+                Color.rgb(0, 208, 255), //Blue-ish
+                Color.rgb(0, 240, 255),
+                Color.rgb(255, 145, 0), //Orange
+                Color.rgb(255, 162, 0),
+                Color.rgb(255, 179, 0),
+                Color.rgb(255, 196, 0),
+                Color.rgb(255, 255, 0)};//Yellow
+        //float [] positions = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        LinearGradient linGrad = new LinearGradient(0, 0, 50, 400, colours, null,
+                Shader.TileMode.MIRROR);
+        //LinearGradient linGrad = new LinearGradient(0, 0, 0, 325, Color.rgb(20, 74, 129),
+        //        Color.rgb(255, 145, 0), Shader.TileMode.MIRROR);
+        paint.setShader(linGrad);
+
+        setupLineChartData();
+
+        // refresh the graph
+        lineChart.invalidate();
+    }
+
+    private void setupLineChart() {
+        lineChart = (LineChart) findViewById(R.id.chart);
+        // disable the description
+        lineChart.setDescription("");
+        //Disable background grid
+        lineChart.getAxisLeft().setDrawGridLines(false);
+        lineChart.getXAxis().setDrawGridLines(false);
+        //lineChart.animateXY(1500, 1500);
+        // disable the legend
+        Legend legend = lineChart.getLegend();
+        legend.setEnabled(false);
+        //Set up x-axis
+        XAxis xAxis = lineChart.getXAxis();
+        xAxis.setEnabled(true);
+        xAxis.setPosition(XAxis.XAxisPosition.TOP);
+        xAxis.setAvoidFirstLastClipping(true);
+        //Set up y-axis on the left
+        YAxis yAxisLeft = lineChart.getAxisLeft();
+        yAxisLeft.setEnabled(true);
+        yAxisLeft.setAxisMinValue(0);
+        yAxisLeft.setAxisMaxValue(11);
+        //Remove y-axis on the right
+        lineChart.getAxisRight().setEnabled(false);
+    }
+
+    private void setupLineChartData() {
         DataBaseHelper dbh = new DataBaseHelper(this);
         ArrayList<Entry> entries = new ArrayList<>();
         ArrayList<String> labels = new ArrayList<>();
@@ -38,32 +106,42 @@ public class GraphActivity extends AppCompatActivity {
 
         cal.set(Calendar.DAY_OF_MONTH, 1);
         cal.set(Calendar.MONTH, 0);
-
         int i = 0;
+        //boolean hasData = false;
         while (!cal.equals(tomorrow)) {
             int rating = dbh.getRating(cal);
-            //String date = "" + cal.get(Calendar.DAY_OF_MONTH) + "/" + (cal.get
-            //        (Calendar.MONTH) + 1) + "/" + cal.get(Calendar.YEAR);
+            String date = "" + cal.get(Calendar.DAY_OF_MONTH) + "/" + (cal.get(Calendar.MONTH) +
+                    1) + "/" + cal.get(Calendar.YEAR);
             if (rating != -1) {
                 entries.add(new Entry(rating, i));
-                labels.add(cal.get(Calendar.DAY_OF_MONTH) + "/" + (cal.get
-                        (Calendar.MONTH) + 1) + "/" + cal.get(Calendar.YEAR));
+                labels.add(date);
+                //hasData = true;
                 i++;
             }
-            else {
-                //No entry
-                //entries.add(new Entry(-1, i));
-            }
-            //labels.add(date);
-            //i++;
+        /*    else if(hasData == true) { //No entry
+                entries.add(new Entry(-1, i));
+                labels.add(date);
+                i++;
+            }*/
+
             cal.add(Calendar.DAY_OF_MONTH, 1);
         }
-        LineDataSet dataset = new LineDataSet(entries, "# of Calls");
+        LineDataSet dataset = new LineDataSet(entries, "");
+        dataset.setLineWidth(5f);
+        dataset.setCubicIntensity(0.1f);
+        dataset.setDrawHorizontalHighlightIndicator(false);
+        dataset.setCircleRadius(5f);
+
         LineData data = new LineData(labels, dataset);
         lineChart.setData(data); // set the data and list of lables into chart
-        lineChart.setDescription("Mood Graph");  // set the description
         dataset.setDrawCubic(true);
-        dataset.setDrawFilled(true);
-        lineChart.invalidate();
+        //dataset.setDrawFilled(true);
+    }
+
+    private void makeToast(String text) {
+        Context context  = getApplicationContext();
+        int     duration = Toast.LENGTH_LONG;
+        Toast   toast    = Toast.makeText(context, text, duration);
+        toast.show();
     }
 }
