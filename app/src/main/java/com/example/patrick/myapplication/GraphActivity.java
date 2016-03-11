345186c3a4786b9f3e6137b421f84749cfd3d2da
ofriele <eoin.ofriel2@mail.dcu.ie>
2016-03-11 19:41:40 +0000

    Graph links to day entry

--- app/src/main/java/com/example/patrick/myapplication/GraphActivity.java
+++ app/src/main/java/com/example/patrick/myapplication/GraphActivity.java
@@ -1,6 +1,5 @@
 package com.example.patrick.myapplication;
 
-import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.LinearGradient;
