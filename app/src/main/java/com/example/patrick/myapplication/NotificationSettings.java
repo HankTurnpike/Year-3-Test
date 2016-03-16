5124e3ecd7fdce2a11994dcd2cb507e627dd72ff
ofriele <eoin.ofriel2@mail.dcu.ie>
2016-03-16 16:07:16 +0000

    commit

--- app/src/main/java/com/example/patrick/myapplication/NotificationSettings.java
+++ app/src/main/java/com/example/patrick/myapplication/NotificationSettings.java
@@ -4,20 +4,23 @@ import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.app.TimePickerDialog;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import java.util.Calendar;
 
 public class NotificationSettings extends AppCompatActivity {
     private PendingIntent pendingIntent;
-
+    private ToggleButton toggle;
     private TextView timeTextView;
     private String time;
     private TimePickerDialog.OnTimeSetListener timePick;
@@ -32,15 +35,33 @@ public class NotificationSettings extends AppCompatActivity {
         SharedPreferences preferences   = PreferenceManager.getDefaultSharedPreferences(this);
         SharedPreferences.Editor editor = preferences.edit();
         String savedTime = preferences.getString("dailyRemTime", "no time selected");
+        boolean savedToggleOn = preferences.getBoolean("savedToggle", false);
+
+
+        /* Retrieve a PendingIntent that will perform a broadcast */
+        Intent alarmIntent = new Intent(this, AlarmReceiver.class);
+        pendingIntent = PendingIntent.getBroadcast(this, 0, alarmIntent, 0);
+        toggle = (ToggleButton) findViewById(R.id.toggleButton_daily_reminder);
+        toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    //Enable alarm
+                    startAlarm();
+                } else {
+                    //Disable alarm
+                }
+            }
+        });
+
         time = "Select time to be reminded: ";
         timeTextView.setText(time);
-
         timePick = new TimePickerDialog.OnTimeSetListener() {
             public void onTimeSet(TimePicker view, int hour, int minute) {
                 //dateAndTime.set(Calendar.HOUR_OF_DAY, hourOfDay);
                 //dateAndTime.set(Calendar.MINUTE, minute);
                 //updateLabel();
                 timeTextView.setText("Time - " + hour + ":" + minute);
+
             }
         };
     }
