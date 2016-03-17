951dce7703d9093cf308ae730ed41a6b884057fe
ofriele <eoin.ofriel2@mail.dcu.ie>
2016-03-17 14:20:13 +0000

    Fixed the month in the title for DateScreen, GraphActivity and MainActivity

--- app/src/main/java/com/example/patrick/myapplication/MainActivity.java
+++ app/src/main/java/com/example/patrick/myapplication/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.content_main);
         Calendar cal = Calendar.getInstance();
         //noinspection ConstantConditions
-        getSupportActionBar().setTitle(cal.get(Calendar.YEAR) + "/" + cal.get(Calendar.MONTH) + "/" + cal.get(Calendar.DAY_OF_MONTH));
+        getSupportActionBar().setTitle(cal.get(Calendar.YEAR) + "/" + (cal.get(Calendar.MONTH)+1) + "/" + cal.get(Calendar.DAY_OF_MONTH));
 
         DataBaseHelper dbh = new DataBaseHelper(this);
         TypedArray ar = this.getResources().obtainTypedArray(R.array.img_id_arr);
