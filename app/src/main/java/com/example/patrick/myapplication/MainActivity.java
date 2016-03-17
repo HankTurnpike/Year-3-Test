62dbd6f7bef3cec0adf276446f6017b2c72b2933
ofriele <eoin.ofriel2@mail.dcu.ie>
2016-03-17 14:24:27 +0000

    Fixed month on MainActivity

--- app/src/main/java/com/example/patrick/myapplication/MainActivity.java
+++ app/src/main/java/com/example/patrick/myapplication/MainActivity.java
@@ -30,7 +30,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.content_main);
         Calendar cal = Calendar.getInstance();
         //noinspection ConstantConditions
-        getSupportActionBar().setTitle(cal.get(Calendar.YEAR) + "/" + cal.get(Calendar.MONTH) + "/" + cal.get(Calendar.DAY_OF_MONTH));
+        getSupportActionBar().setTitle(cal.get(Calendar.YEAR) + "/" + (cal.get(Calendar.MONTH)+1) +
+                "/" + cal.get(Calendar.DAY_OF_MONTH));
         DataBaseHelper dbh = new DataBaseHelper(this);
         //Get array of rating drawable locations.
         TypedArray ar = this.getResources().obtainTypedArray(R.array.img_id_arr);
